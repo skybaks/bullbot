32aaa2e2e63103798a14dbd0c83bf0c449219806
TroyDota <troybensonsa@gmail.com>
2020-01-16 15:50:33 +0200

    Moved migration

--- pajbot/migration_revisions/db/0013_user_connections_modify.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-log = logging.getLogger("pajbot")
-
-
-def up(cursor, bot):
-    # new: twitch_tier record
-    cursor.execute('ALTER TABLE "user_connections" ADD COLUMN discord_tier INTEGER DEFAULT NULL')
