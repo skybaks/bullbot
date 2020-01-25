37f3d97ae3ee9b3a95abcd1ae8c4c8ddbadd8db5
TroyDota <troybensonsa@gmail.com>
2020-01-25 17:12:03 +0000

    reformat

--- pajbot/models/user_connection.py
+++ pajbot/models/user_connection.py
@@ -91,13 +91,17 @@ class UserConnections(Base):
 
     @staticmethod
     def _by_tier(db_session, tier):
-        return db_session.query(UserConnections).filter(UserConnections.tier==tier).order_by(UserConnections.twitch_login).all()
+        return (
+            db_session.query(UserConnections)
+            .filter(UserConnections.tier == tier)
+            .order_by(UserConnections.twitch_login)
+            .all()
+        )
 
     @staticmethod
     def _count_by_tier(db_session, tier):
-        return db_session.query(func.count(UserConnections.twitch_id)).filter(UserConnections.tier==tier).scalar()
+        return db_session.query(func.count(UserConnections.twitch_id)).filter(UserConnections.tier == tier).scalar()
 
     @staticmethod
     def _count(db_session):
         return db_session.query(func.count(UserConnections.twitch_id)).scalar()
-
